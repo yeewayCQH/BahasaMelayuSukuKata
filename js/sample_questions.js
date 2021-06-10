ccdfc30f34f685224b7144dd8e8f1b3f6a65194b
yeewayCQH <yeewayncs@gmail.com>
2021-06-10 20:58:16 +0800

    add sample html

---
+++ js/sample_questions.js
@@ -0,0 +1,97 @@
+
+let questions_1 = [
+
+{
+    pics: "../image/unit01_soalan01.jpg",
+    answer: "sayang",
+    options: [
+      "sayang",
+      "keluarga",
+      "sebut",
+      "ajuk",
+      "bunyi"
+    ]
+  },
+
+  {
+      pics: "../image/unit01_soalan02.jpg",
+      answer: "keluarga",
+      options: [
+        "sayang",
+        "keluarga",
+        "sebut",
+        "ajuk",
+        "bunyi"
+      ]
+  },
+
+  {
+    pics: "../image/sample/huruf_vokal_api.jpg",
+    answer: "a",
+    options: [
+      "a",
+      "u",
+      "i",
+      "e",
+      "o"
+    ]
+  },
+    {
+    pics: "../image/sample/huruf_vokal_ayam.jpg",
+    answer: "a",
+    options: [
+      "a",
+      "e",
+      "i",
+      "o",
+      "u"
+    ]
+  },
+
+  {
+      pics: "../image/sample/huruf_vokal_itik2.jpg",
+      answer: "itik",
+      options: [
+        "itik",
+        "ulat",
+        "api",
+        "ikan",
+        "api"
+      ]
+    },
+      {
+      pics: "../image/sample/huruf_vokal_ikan2.jpg",
+      answer: "ikan",
+      options: [
+        "ikan",
+        "otak",
+        "ibu",
+        "abang",
+        "enam"
+      ]
+  },
+
+  {
+      pics: "../image/sample/count_01.jpg",
+      answer: "4",
+      options: [
+        "3",
+        "4",
+        "5",
+        "6",
+        "7"
+      ]
+    },
+      {
+      pics: "../image/sample/add_01.jpg",
+      answer: "5",
+      options: [
+        "5",
+        "2",
+        "3",
+        "7",
+        "6"
+      ]
+  },
+
+];
