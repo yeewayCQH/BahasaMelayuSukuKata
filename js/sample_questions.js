6bc0d36a24ed9d7c10656084147ed75733f60ff0
yeewayCQH <yeewayncs@gmail.com>
2021-06-12 22:56:38 +0800

    amend sample

--- js/sample_questions.js
+++ js/sample_questions.js
@@ -24,74 +24,17 @@ let questions_1 = [
         "bunyi"
       ]
   },
-
   {
-    pics: "../image/sample/huruf_vokal_api.JPG",
-    answer: "a",
-    options: [
-      "a",
-      "u",
-      "i",
-      "e",
-      "o"
-    ]
-  },
-    {
-    pics: "../image/sample/huruf_vokal_ayam.JPG",
-    answer: "a",
-    options: [
-      "a",
-      "e",
-      "i",
-      "o",
-      "u"
-    ]
-  },
-
-  {
-      pics: "../image/sample/huruf_vokal_itik2.JPG",
-      answer: "itik",
+      pics: "../image/sample/unit01_soalan04.jpg",
+      answer: "sebut",
       options: [
-        "itik",
-        "ulat",
-        "api",
-        "ikan",
-        "api"
-      ]
-    },
-      {
-      pics: "../image/sample/huruf_vokal_ikan2.JPG",
-      answer: "ikan",
-      options: [
-        "ikan",
-        "otak",
-        "ibu",
-        "abang",
-        "enam"
+        "sayang",
+        "keluarga",
+        "sebut",
+        "ajuk",
+        "bunyi"
       ]
   },
 
-  {
-      pics: "../image/sample/count_01.JPG",
-      answer: "4",
-      options: [
-        "3",
-        "4",
-        "5",
-        "6",
-        "7"
-      ]
-    },
-      {
-      pics: "../image/sample/add_01.JPG",
-      answer: "5",
-      options: [
-        "5",
-        "2",
-        "3",
-        "7",
-        "6"
-      ]
-  },
 
 ];
