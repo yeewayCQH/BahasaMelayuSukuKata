a597420f6208a5381965b4b43b4abdd0dbff6c2a
yeewayCQH <yeewayncs@gmail.com>
2021-10-01 19:10:07 +0800

    add reading page
    add reading page

---
+++ js/Reading.js
@@ -0,0 +1,301 @@
+
+/****************************************************************
+ *                        Chinese Disney Story                  *
+ ****************************************************************/
+
+// ============================= Level 1
+const menu_disneyL1_book1   = document.getElementById("menu_disneyL1_book1");
+const menu_disneyL1_book2   = document.getElementById("menu_disneyL1_book2");
+const menu_disneyL1_book3   = document.getElementById("menu_disneyL1_book3");
+const menu_disneyL1_book4   = document.getElementById("menu_disneyL1_book4");
+const menu_disneyL1_book5   = document.getElementById("menu_disneyL1_book5");
+const menu_disneyL1_book6   = document.getElementById("menu_disneyL1_book6");
+
+menu_disneyL1_book1.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/ysfx/");
+
+});
+
+menu_disneyL1_book2.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/njfi/");
+
+});
+
+menu_disneyL1_book3.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/xtod/");
+
+});
+
+menu_disneyL1_book4.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/gztr/");
+
+});
+
+menu_disneyL1_book5.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/xrog/");
+
+});
+
+menu_disneyL1_book6.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/dlro/");
+
+});
+
+// ============================= Level 2
+
+const menu_disneyL2_book1   = document.getElementById("menu_disneyL2_book1");
+const menu_disneyL2_book2   = document.getElementById("menu_disneyL2_book2");
+const menu_disneyL2_book3   = document.getElementById("menu_disneyL2_book3");
+const menu_disneyL2_book4   = document.getElementById("menu_disneyL2_book4");
+const menu_disneyL2_book5   = document.getElementById("menu_disneyL2_book5");
+const menu_disneyL2_book6   = document.getElementById("menu_disneyL2_book6");
+
+menu_disneyL2_book1.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/zhsa/");
+
+});
+
+menu_disneyL2_book2.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/inie/");
+
+});
+
+menu_disneyL2_book3.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/npru/");
+
+});
+
+menu_disneyL2_book4.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/exmy/");
+
+});
+
+menu_disneyL2_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL2_book6.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+// ============================= Level 3
+
+const menu_disneyL3_book1   = document.getElementById("menu_disneyL3_book1");
+const menu_disneyL3_book2   = document.getElementById("menu_disneyL3_book2");
+const menu_disneyL3_book3   = document.getElementById("menu_disneyL3_book3");
+const menu_disneyL3_book4   = document.getElementById("menu_disneyL3_book4");
+const menu_disneyL3_book5   = document.getElementById("menu_disneyL3_book5");
+const menu_disneyL3_book6   = document.getElementById("menu_disneyL3_book6");
+
+menu_disneyL3_book1.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL3_book2.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL3_book3.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL3_book4.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL3_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL3_book6.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+// ============================= Level 4
+
+const menu_disneyL4_book1   = document.getElementById("menu_disneyL4_book1");
+const menu_disneyL4_book2   = document.getElementById("menu_disneyL4_book2");
+const menu_disneyL4_book3   = document.getElementById("menu_disneyL4_book3");
+const menu_disneyL4_book4   = document.getElementById("menu_disneyL4_book4");
+const menu_disneyL4_book5   = document.getElementById("menu_disneyL4_book5");
+const menu_disneyL4_book6   = document.getElementById("menu_disneyL4_book6");
+
+menu_disneyL4_book1.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL4_book2.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL4_book3.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL4_book4.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL4_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL4_book6.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+// ============================= Level 5
+
+const menu_disneyL5_book1   = document.getElementById("menu_disneyL5_book1");
+const menu_disneyL5_book2   = document.getElementById("menu_disneyL5_book2");
+const menu_disneyL5_book3   = document.getElementById("menu_disneyL5_book3");
+const menu_disneyL5_book4   = document.getElementById("menu_disneyL5_book4");
+const menu_disneyL5_book5   = document.getElementById("menu_disneyL5_book5");
+const menu_disneyL5_book6   = document.getElementById("menu_disneyL5_book6");
+
+menu_disneyL1_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL1_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL1_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL1_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL1_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL1_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+// ============================= Level 6
+
+const menu_disneyL6_book1   = document.getElementById("menu_disneyL6_book1");
+const menu_disneyL6_book2   = document.getElementById("menu_disneyL6_book2");
+const menu_disneyL6_book3   = document.getElementById("menu_disneyL6_book3");
+const menu_disneyL6_book4   = document.getElementById("menu_disneyL6_book4");
+const menu_disneyL6_book5   = document.getElementById("menu_disneyL6_book5");
+const menu_disneyL6_book6   = document.getElementById("menu_disneyL6_book6");
+
+menu_disneyL6_book1.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL6_book2.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL6_book3.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL6_book4.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL6_book5.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+menu_disneyL6_book6.addEventListener("click", function(){
+
+	window.open("");
+
+});
+
+
+/****************************************************************
+ *                        Science                               *
+ ****************************************************************/
+
+const menu_ScienceAdventures_1   = document.getElementById("menu_ScienceAdventures_1");
+const menu_ScienceAdventures_2   = document.getElementById("menu_ScienceAdventures_2");
+const menu_ScienceAdventures_3   = document.getElementById("menu_ScienceAdventures_3");
+
+menu_ScienceAdventures_1.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/tqux/#p=1");
+
+});
+
+menu_ScienceAdventures_2.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/pwtji/izjj/#p=1");
+
+});
+
+menu_ScienceAdventures_3.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/fayj/#p=1");
+
+});
+
