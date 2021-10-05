0d819d9db4d655c8e57ea61cd9ba4ee29a635654
yeewayCQH <yeewayncs@gmail.com>
2021-10-05 14:50:29 +0800

    add more book
    add more book

--- js/Reading.js
+++ js/Reading.js
@@ -137,7 +137,7 @@ menu_disneyL3_book6.addEventListener("click", function(){
 
 });
 
-/*
+
 // ============================= Level 4
 
 const menu_disneyL4_book1   = document.getElementById("menu_disneyL4_book1");
@@ -149,37 +149,37 @@ const menu_disneyL4_book6   = document.getElementById("menu_disneyL4_book6");
 
 menu_disneyL4_book1.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/wsab/");
 
 });
 
 menu_disneyL4_book2.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/inav/");
 
 });
 
 menu_disneyL4_book3.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/owid/");
 
 });
 
 menu_disneyL4_book4.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/rjyan/ykvf/");
 
 });
 
 menu_disneyL4_book5.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/rjyan/embh/");
 
 });
 
 menu_disneyL4_book6.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/rjyan/bdtr/");
 
 });
 
@@ -192,39 +192,39 @@ const menu_disneyL5_book4   = document.getElementById("menu_disneyL5_book4");
 const menu_disneyL5_book5   = document.getElementById("menu_disneyL5_book5");
 const menu_disneyL5_book6   = document.getElementById("menu_disneyL5_book6");
 
-menu_disneyL1_book5.addEventListener("click", function(){
+menu_disneyL5_book1.addEventListener("click", function(){
 
-	window.open("");
-
-});
-
-menu_disneyL1_book5.addEventListener("click", function(){
-
-	window.open("");
+	window.open("https://online.fliphtml5.com/pwtji/okxo/");
 
 });
 
-menu_disneyL1_book5.addEventListener("click", function(){
+menu_disneyL5_book2.addEventListener("click", function(){
 
-	window.open("");
-
-});
-
-menu_disneyL1_book5.addEventListener("click", function(){
-
-	window.open("");
+	window.open("https://online.fliphtml5.com/pwtji/qxpd/");
 
 });
 
-menu_disneyL1_book5.addEventListener("click", function(){
+menu_disneyL5_book3.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/wlel/");
 
 });
 
-menu_disneyL1_book5.addEventListener("click", function(){
+menu_disneyL5_book4.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/evop/");
+
+});
+
+menu_disneyL5_book5.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/ednp/");
+
+});
+
+menu_disneyL5_book6.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/oqja/");
 
 });
 
@@ -239,40 +239,40 @@ const menu_disneyL6_book6   = document.getElementById("menu_disneyL6_book6");
 
 menu_disneyL6_book1.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/nkkr/");
 
 });
 
 menu_disneyL6_book2.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/qgoc/");
 
 });
 
 menu_disneyL6_book3.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/uyswk/fiin/");
 
 });
 
 menu_disneyL6_book4.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/pwtji/nqho/");
 
 });
 
 menu_disneyL6_book5.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/pwtji/plqo/");
 
 });
 
 menu_disneyL6_book6.addEventListener("click", function(){
 
-	window.open("");
+	window.open("https://online.fliphtml5.com/pwtji/afwj/");
 
 });
-*/
+
 
 
 /****************************************************************
@@ -303,6 +303,14 @@ const menu_doraemon_book7  = document.getElementById("menu_doraemon_book7");
 const menu_doraemon_book8  = document.getElementById("menu_doraemon_book8");
 const menu_doraemon_book9  = document.getElementById("menu_doraemon_book9");
 const menu_doraemon_book10 = document.getElementById("menu_doraemon_book10");
+const menu_doraemon_book11 = document.getElementById("menu_doraemon_book11");
+const menu_doraemon_book12 = document.getElementById("menu_doraemon_book12");
+const menu_doraemon_book13 = document.getElementById("menu_doraemon_book13");
+const menu_doraemon_book14 = document.getElementById("menu_doraemon_book14");
+const menu_doraemon_book15 = document.getElementById("menu_doraemon_book15");
+const menu_doraemon_book16 = document.getElementById("menu_doraemon_book16");
+const menu_doraemon_book17 = document.getElementById("menu_doraemon_book17");
+const menu_doraemon_book18 = document.getElementById("menu_doraemon_book18");
 
 menu_doraemon_book1.addEventListener("click", function(){
 
@@ -312,7 +320,7 @@ menu_doraemon_book1.addEventListener("click", function(){
 
 menu_doraemon_book2.addEventListener("click", function(){
 
- 	window.open("");
+ 	window.open("https://online.fliphtml5.com/pwtji/mhek/");
 
 });
 
@@ -324,13 +332,13 @@ menu_doraemon_book3.addEventListener("click", function(){
 
 menu_doraemon_book4.addEventListener("click", function(){
 
- 	window.open("");
+ 	window.open("https://online.fliphtml5.com/pwtji/bnwf/");
 
 });
 
 menu_doraemon_book5.addEventListener("click", function(){
 
- 	window.open("");
+ 	window.open("https://online.fliphtml5.com/pwtji/xibg/");
 
 });
 
@@ -342,7 +350,7 @@ menu_doraemon_book6.addEventListener("click", function(){
 
 menu_doraemon_book7.addEventListener("click", function(){
 
- 	window.open("");
+ 	window.open("https://online.fliphtml5.com/pwtji/vems/");
 
 });
 
@@ -364,6 +372,53 @@ menu_doraemon_book10.addEventListener("click", function(){
 
 });
 
+menu_doraemon_book11.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/pwtji/lzqy/");
+
+});
+
+menu_doraemon_book12.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/rjyan/hxsm/");
+
+});
+
+menu_doraemon_book13.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/rjyan/ktnw/");
+
+});
+
+menu_doraemon_book14.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/uyswk/zphc/");
+
+});
+
+menu_doraemon_book15.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/uyswk/smnv/");
+
+});
+
+menu_doraemon_book16.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/uyswk/tonk/");
+
+});
+
+menu_doraemon_book17.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/pwtji/futh/");
+
+});
+
+menu_doraemon_book18.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/pwtji/hmug/");
+
+});
 
 
 /****************************************************************
@@ -391,4 +446,47 @@ menu_ScienceAdventures_3.addEventListener("click", function(){
 	window.open("https://online.fliphtml5.com/pwtji/fayj/#p=1");
 
 });
+
+const menu_EnglishPicDictionary = document.getElementById("menu_EnglishPicDictionary");
+const  menu_PhonicsBook1        = document.getElementById("menu_PhonicsBook1");
+const  menu_PhonicsBook2        = document.getElementById("menu_PhonicsBook2");
+
+menu_EnglishPicDictionary.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/pafs/");
+
+});
+
+menu_PhonicsBook1.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/taeq/");
+
+});
+
+menu_PhonicsBook2.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/pwtji/zqls/");
+
+});
+
+
+/****************************************************************
+ *                        Donald Duck                           *
+ ****************************************************************/
+
+const menu_DonaldDuck_1   = document.getElementById("menu_DonaldDuck_1");
+const menu_DonaldDuck_2   = document.getElementById("menu_DonaldDuck_2");
+
+menu_DonaldDuck_1.addEventListener("click", function(){
+
+	window.open("https://online.fliphtml5.com/uyswk/jfqg/");
+
+});
+
+menu_DonaldDuck_2.addEventListener("click", function(){
+
+ 	window.open("https://online.fliphtml5.com/uyswk/jywz/");
+
+});
+
 
