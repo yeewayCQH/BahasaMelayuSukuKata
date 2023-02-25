065302ec34a2b22aa1595658fef93e97eb4d043e
yeewayCQH <yeewayncs@gmail.com>
2023-02-25 17:37:42 +0800

    add elephant & pigggie

--- comic/js/bookAudio_data.js
+++ comic/js/bookAudio_data.js
@@ -2,6 +2,656 @@
 let AllBooks = [
 
 
+// ========================================================== An Elephant & Piggie ==========================================
+
+  {
+      id: "GPBook01",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/zduv/",
+      storyMP3: "comic/audio/GPBook01.mp3",
+      songMP3:  "",
+      videoMP4: "comic/video/GPBook01 Are you ready to play outside.mp4"
+  },
+  {
+      id: "GPBook02",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/ciim/",
+      storyMP3: "comic/audio/GPBook02.mp3",
+      songMP3:  "",
+      videoMP4: "comic/video/GPBook02 Today I will fly.mp4"
+  },
+  {
+      id: "GPBook03",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/tvio/",
+      storyMP3: "comic/audio/GPBook03.mp3",
+      songMP3:  "",
+      videoMP4: "comic/video/GPBook03 Elephant Cannot Dance.mp4"
+  },
+  {
+      id: "GPBook04",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/wljw/",
+      storyMP3: "comic/audio/GPBook04.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook05",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/fxvg/",
+      storyMP3: "comic/audio/GPBook05.mp3",
+      songMP3:  "",
+      videoMP4: "comic/video/GPBook05 I love my new toy.mp4"
+  },
+  {
+      id: "GPBook06",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/vber/",
+      storyMP3: "comic/audio/GPBook06.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook07",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/rwha/",
+      storyMP3: "comic/audio/GPBook07.mp3",
+      songMP3:  "",
+      videoMP4: "comic/video/GPBook07 Can I play too.mp4"
+  },
+  {
+      id: "GPBook08",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/ohzd/",
+      storyMP3: "comic/audio/GPBook08.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook09",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/cxem/",
+      storyMP3: "comic/audio/GPBook09.mp3",
+      songMP3:  "",
+      videoMP4: "comic/video/GPBook09 My friend is sad.mp4"
+  },
+  {
+      id: "GPBook10",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/dbgx/",
+      storyMP3: "comic/audio/GPBook10.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook11",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/lcfh/",
+      storyMP3: "comic/audio/GPBook11.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook12",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/cnag/",
+      storyMP3: "comic/audio/GPBook12.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook13",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/mwwv/",
+      storyMP3: "comic/audio/GPBook13.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook14",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/xagp/",
+      storyMP3: "comic/audio/GPBook14.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook15",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/xpiu/",
+      storyMP3: "comic/audio/GPBook15.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook16",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/fuwz/",
+      storyMP3: "comic/audio/GPBook16.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook17",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/vxrs/",
+      storyMP3: "comic/audio/GPBook17.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook18",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/eapg/",
+      storyMP3: "comic/audio/GPBook18.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook19",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/lepw/",
+      storyMP3: "comic/audio/GPBook19.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook20",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/zyfx/",
+      storyMP3: "comic/audio/GPBook20.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook21",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/oqhf/",
+      storyMP3: "comic/audio/GPBook21.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook22",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/xnvp/",
+      storyMP3: "comic/audio/GPBook22.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook23",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/lfvl/",
+      storyMP3: "comic/audio/GPBook23.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook24",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/rlwe/",
+      storyMP3: "comic/audio/GPBook24.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+  {
+      id: "GPBook25",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/jcrn/",
+      storyMP3: "comic/audio/GPBook25.mp3",
+      songMP3:  "",
+      videoMP4: ""
+  },
+
+
+  // ================================================== YI TONG ==================================================
+  // YI TONG Level 1
+  {
+      id: "YiTongL1B1",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/naos/",
+      storyMP3: "comic/audio/YiTongL1B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL1B2",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/vyky/",
+      storyMP3: "comic/audio/YiTongL1B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL1B3",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/rdoq/",
+      storyMP3: "comic/audio/YiTongL1B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL1B4",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/iwfc/",
+      storyMP3: "comic/audio/YiTongL1B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL1B5",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/rowf/",
+      storyMP3: "comic/audio/YiTongL1B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL1B6",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/ppkg/",
+      storyMP3: "comic/audio/YiTongL1B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL1B7",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/unbv/",
+      storyMP3: "comic/audio/YiTongL1B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL1B8",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/jojp/",
+      storyMP3: "comic/audio/YiTongL1B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL1B8_song.mp3",
+      videoMP4: ""
+  },
+
+  // YI TONG Level 2
+  {
+      id: "YiTongL2B1",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/lpxa/",
+      storyMP3: "comic/audio/YiTongL2B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL2B2",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/tczg/",
+      storyMP3: "comic/audio/YiTongL2B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL2B3",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/pfny/",
+      storyMP3: "comic/audio/YiTongL2B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL2B4",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/oocp/",
+      storyMP3: "comic/audio/YiTongL2B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL2B5",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/tfkr/",
+      storyMP3: "comic/audio/YiTongL2B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL2B6",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/ayha/",
+      storyMP3: "comic/audio/YiTongL2B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL2B7",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/yilt/",
+      storyMP3: "comic/audio/YiTongL2B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL2B8",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/ijqk/",
+      storyMP3: "comic/audio/YiTongL2B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL2B8_song.mp3",
+      videoMP4: ""
+  },
+
+
+  // YI TONG Level 3
+  {
+      id: "YiTongL3B1",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/bjmp/",
+      storyMP3: "comic/audio/YiTongL3B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL3B2",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/iwce/",
+      storyMP3: "comic/audio/YiTongL3B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL3B3",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/uuig/",
+      storyMP3: "comic/audio/YiTongL3B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL3B4",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/pawh/",
+      storyMP3: "comic/audio/YiTongL3B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL3B5",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/cizj/",
+      storyMP3: "comic/audio/YiTongL3B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL3B6",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/igji/",
+      storyMP3: "comic/audio/YiTongL3B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL3B7",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/pgms/",
+      storyMP3: "comic/audio/YiTongL3B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL3B8",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/rsul/",
+      storyMP3: "comic/audio/YiTongL3B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL3B8_song.mp3",
+      videoMP4: ""
+  },
+
+
+  // YI TONG Level 4
+  {
+      id: "YiTongL4B1",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/wfyo/",
+      storyMP3: "comic/audio/YiTongL4B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL4B2",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/grua/",
+      storyMP3: "comic/audio/YiTongL4B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL4B3",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/sxsn/",
+      storyMP3: "comic/audio/YiTongL4B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL4B4",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/nrwv/",
+      storyMP3: "comic/audio/YiTongL4B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL4B5",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/anxb/",
+      storyMP3: "comic/audio/YiTongL4B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL4B6",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/mpea/",
+      storyMP3: "comic/audio/YiTongL4B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL4B7",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/nqdy/",
+      storyMP3: "comic/audio/YiTongL4B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL4B8",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/nvfz/",
+      storyMP3: "comic/audio/YiTongL4B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL4B8_song.mp3",
+      videoMP4: ""
+  },
+
+
+  // YI TONG Level 5
+  {
+      id: "YiTongL5B1",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/dfpx/",
+      storyMP3: "comic/audio/YiTongL5B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL5B2",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/uuss/",
+      storyMP3: "comic/audio/YiTongL5B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL5B3",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/blar/",
+      storyMP3: "comic/audio/YiTongL5B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL5B4",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/ufwb/",
+      storyMP3: "comic/audio/YiTongL5B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL5B5",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/cghf/",
+      storyMP3: "comic/audio/YiTongL5B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL5B6",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/elud/",
+      storyMP3: "comic/audio/YiTongL5B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL5B7",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/lbog/",
+      storyMP3: "comic/audio/YiTongL5B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL5B8",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/zrui/",
+      storyMP3: "comic/audio/YiTongL5B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL5B8_song.mp3",
+      videoMP4: ""
+  },
+
+
+  // YI TONG Level 6
+  {
+      id: "YiTongL6B1",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/qvtb/",
+      storyMP3: "comic/audio/YiTongL6B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL6B2",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/bujs/",
+      storyMP3: "comic/audio/YiTongL6B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL6B3",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/ofna/",
+      storyMP3: "comic/audio/YiTongL6B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL6B4",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/yofc/",
+      storyMP3: "comic/audio/YiTongL6B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL6B5",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/oclt/",
+      storyMP3: "comic/audio/YiTongL6B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL6B6",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/gaqb/",
+      storyMP3: "comic/audio/YiTongL6B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL6B7",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/wpkp/",
+      storyMP3: "comic/audio/YiTongL6B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL6B8",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/fgzi/",
+      storyMP3: "comic/audio/YiTongL6B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL6B8_song.mp3",
+      videoMP4: ""
+  },
+
+
+  // YI TONG Level 7
+  {
+      id: "YiTongL7B1",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/dwrl/",
+      storyMP3: "comic/audio/YiTongL7B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL7B2",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/nzgt/",
+      storyMP3: "comic/audio/YiTongL7B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL7B3",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/mlvx/",
+      storyMP3: "comic/audio/YiTongL7B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL7B4",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/hqwq/",
+      storyMP3: "comic/audio/YiTongL7B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL7B5",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/cwrx/",
+      storyMP3: "comic/audio/YiTongL7B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL7B6",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/izrb/",
+      storyMP3: "comic/audio/YiTongL7B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL7B7",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/hlpz/",
+      storyMP3: "comic/audio/YiTongL7B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL7B8",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/muvr/",
+      storyMP3: "comic/audio/YiTongL7B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL7B8_song.mp3",
+      videoMP4: ""
+  },
+
+
+  // YI TONG Level 8
+  {
+      id: "YiTongL8B1",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/bmjv/",
+      storyMP3: "comic/audio/YiTongL8B1_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B1_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL8B2",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/nhhr/",
+      storyMP3: "comic/audio/YiTongL8B2_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B2_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL8B3",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/otjw/",
+      storyMP3: "comic/audio/YiTongL8B3_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B3_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL8B4",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/gpsf/",
+      storyMP3: "comic/audio/YiTongL8B4_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B4_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL8B5",
+      flipBookURL: "https://online.fliphtml5.com/uyswk/frwt/",
+      storyMP3: "comic/audio/YiTongL8B5_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B5_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL8B6",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/iifb/",
+      storyMP3: "comic/audio/YiTongL8B6_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B6_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL8B7",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/vpwv/",
+      storyMP3: "comic/audio/YiTongL8B7_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B7_song.mp3",
+      videoMP4: ""
+  },
+  {
+      id: "YiTongL8B8",
+      flipBookURL: "https://online.fliphtml5.com/pwtji/vhgf/",
+      storyMP3: "comic/audio/YiTongL8B8_story.mp3",
+      songMP3:  "comic/audio/YiTongL8B8_song.mp3",
+      videoMP4: ""
+  },
+
 // Reading Journey Standard 1 Level 1
   {
       id: "RJS1L1B1",
